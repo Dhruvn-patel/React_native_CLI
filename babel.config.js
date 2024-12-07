037bd3b5debc77e507c5963a16284f1e15d05ece
Dhruvn-patel <dhruvpateldhruvpatel01669@gmail.com>
2024-12-07 14:05:59 +0530

    init cli setup

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,46 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset', '@babel/preset-typescript'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'ENV',
+        moduleName: '@env',
+        path: '.env',
+        blocklist: null,
+        allowlist: null,
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
+    [
+      'module-resolver',
+      {
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.ios.jsx',
+          '.android.jsx',
+          '.js',
+          '.jsx',
+          '.json',
+          '.ts',
+          '.tsx',
+        ],
+        root: ['.'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@navigation': './src/navigation',
+          '@config': './src/config',
+          '@hooks': './src/hooks',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/navigation/authNaigation/index.tsx
@@ -0,0 +1,7 @@
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+
+const Navigation = () => {
+  return <SafeAreaProvider></SafeAreaProvider>;
+};
+
+export default Navigation;
